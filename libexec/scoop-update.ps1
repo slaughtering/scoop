ed4c22898293bcaf9971bb2633aa28d6c37073fb
slaughtering <membogg@qq.com>
2022-04-01 12:43:51 +0800

    change repo url from hub.fastgit.org back to github.com

--- libexec/scoop-update.ps1
+++ libexec/scoop-update.ps1
@@ -39,7 +39,7 @@ $independent = $opt.i -or $opt.independent
 # load config
 $configRepo = get_config SCOOP_REPO
 if (!$configRepo) {
-    $configRepo = "https://hub.fastgit.org/slaughtering/scoop"
+    $configRepo = "https://github.com/slaughtering/scoop"
     set_config SCOOP_REPO $configRepo | Out-Null
 }
 
